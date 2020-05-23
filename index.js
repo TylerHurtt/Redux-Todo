0dad85d72b53d801a9897f2b1605428b7354585c
Sweet T <thurtt@rocketmail.com>
2020-05-22 21:39:47 -0700

    added reducer function

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 // Action types
-const actionTypes = [
-  {
-    type: 'ADD_TODO',
-    todo: {
-      id: 0,
-      name: 'learn Redux',
-      complete: false,
-    },
-  },
-  {
-    type: 'REMOVE_TODO',
+{
+  type: 'ADD_TODO',
+  todo: {
     id: 0,
-  },
-  {
-    type: 'TOGGLE_TODO',
+    name: 'Learn Redux',
+    complete: false,
+  }
+}
+
+{
+  type: 'REMOVE_TODO',
+  id: 0,
+}
+
+{
+  type: 'TOGGLE_TODO',
+  id: 0,
+}
+
+{
+  type: 'ADD_GOAL',
+  goal: {
     id: 0,
-  },
-  {
-    type: 'ADD_GOAL',
-    goal: {
-      id: 0,
-      name: 'Run a marathon',
-      complete: false,
-    },
-  },
-  {
-    type: 'REMOVE_GOAL',
-    id: 0,
-  },
-];
+    name: 'Run a Marathon'
+  }
+}
+
+{
+  type: 'REMOVE_GOAL',
+  id: 0
+}
+
+function todos(state = [], action) {
+  if(action.type === 'ADD_TODO') 
+    return state.concat([action.todo])
+
+  return state
+}
 
 function createStore() {
   // 1. The state.
