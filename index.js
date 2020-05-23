0c51b2ddc7e0a56aa27d78635b8a740fedb46989
Sweet T <thurtt@rocketmail.com>
2020-05-22 20:12:28 -0700

    Added listening to cs

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 function createStore() {
   // 1. The state.
   let state;
+  // Array of listener functions
+  let listeners = [];
   // 2. Get State.
-  const getState = () => {};
-
+  const getState = () => state;
+  // 3. Listen for state changes.
+  const subscribe = (listener) => {
+    // push listener callback into listeners array
+    listeners.push(listener);
+    // return unsubscribe function
+    return () => {
+      // remove listener from listeners array
+      listeners.filter((l) => l !== listener);
+    };
+  };
   return {
     getState,
+    subscribe,
   };
-  // 3. Listen for state changes.
   // 4. Update state.
 }
