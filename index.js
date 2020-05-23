d523532cb5fc8280c381e9d44b2abf9868d1136e
Sweet T <thurtt@rocketmail.com>
2020-05-22 19:59:36 -0700

    setup createStore and getState function

---
+++ index.js
@@ -0,0 +1,12 @@
+function createStore() {
+  // 1. The state.
+  let state;
+  // 2. Get State.
+  const getState = () => {};
+
+  return {
+    getState,
+  };
+  // 3. Listen for state changes.
+  // 4. Update state.
+}
